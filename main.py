e6606f50ac1dc4ecf7c19359dc6009623764b609
peyro <peyrovskaaa@gmail.com>
2025-07-02 09:54:03 +0200

    Refactor caching implementation in main.py to use async Redis client and simplify key builder

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from typing import List, Optional, Union
 import redis
 import uvicorn
 from fastapi import FastAPI, Request, Query, Depends
-from fastapi_cache import FastAPICache, default_key_builder
+from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, Response
 
 from fastapi_cache.decorator import cache
 
@@ -22,11 +22,15 @@ api: AuctionApiClient | None = None
 async def lifespan(app: FastAPI):
     global api
     api = AuctionApiClient()
-    redis_client = redis.Redis.from_url(REDIS_URL)
+    redis_client = redis.asyncio.Redis.from_url(REDIS_URL)
     FastAPICache.init(RedisBackend(redis_client), prefix="fastapi-cache")
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+
+def default_key_builder(func, namespace, request: Request, response: Response):
+    return f"{namespace}:{request.url}"
 
 @app.exception_handler(BadRequestException)
 async def bad_request_exception_handler(request: Request, exc: BadRequestException):
@@ -36,11 +40,12 @@ async def bad_request_exception_handler(request: Request, exc: BadRequestExcepti
     )
 
 @app.get("/cars/", response_model=List[BasicLot])
-@cache(expire=60*60*24, key_builder=default_key_builder)
+@cache(expire=60*60*24)
 async def get_by_lot_id_or_vin(
-    site: Optional[str] = Query(default=None),
+    site: Optional[str] = Query(default=''),
     vin_or_lot: str = Query(...),
 ):
+
     data = VinOrLotIn(site=site, vin_or_lot=vin_or_lot)
 
     vin_or_lots = data.vin_or_lot.replace(" ", "").upper()
@@ -52,36 +57,21 @@ async def get_by_lot_id_or_vin(
         in_data = LotByVINIn(vin=vin_or_lots, site=data.site)
         return await api.request_with_schema(api.GET_LOT_BY_VIN_FOR_ALL_TIME, in_data)
 
-# @app.get("/cars/", response_model=List[BasicLot] | List[BasicHistoryLot])
-# @cache(expire=60*60*24, key_builder=default_key_builder)
-# async def get_by_lot_id_or_vin(
-#     site: Optional[Union[int, str]] = Query(None),
-#     vin_or_lot: str = Query(...),
-# ):
-#     data = VinOrLotIn(site=site, vin_or_lot=vin_or_lot)
-#
-#     vin_or_lots = data.vin_or_lot.replace(" ", "").upper()
-#
-#     if vin_or_lots.isdigit():
-#         in_data = LotByIDIn(lot_id=int(vin_or_lots), site=data.site)
-#         return await api.request_with_schema(api.GET_LOT_BY_ID_FOR_ALL_TIME, in_data)
-#     else:
-#         in_data = LotByVINIn(vin=vin_or_lots, site=data.site)
-#         return await api.request_with_schema(api.GET_LOT_BY_VIN_FOR_ALL_TIME, in_data)
+
 
 @app.get("/cars/current-bid/", response_model=CurrentBidOut)
-@cache(expire=60*60*24, key_builder=default_key_builder)
+@cache(expire=60*60*24)
 async def get_current_bid(data: LotByIDIn = Query()):
     return await api.request_with_schema(api.GET_CURRENT_BID_FOR_LOT, data)
 
 @app.get("/cars/history/vin/", response_model=BasicHistoryLot)
-@cache(expire=60*60*24, key_builder=default_key_builder)
+@cache(expire=60*60*24)
 async def get_history_by_vin(vin: str = Query(...), site: str = Query(...)):
     data = LotByVINIn(vin=vin, site=site)
     return await api.request_with_schema(AuctionApiClient.GET_LOT_HISTORY_BY_VIN, data)
 
 @app.get("/cars/history/lot-id/", response_model=BasicHistoryLot)
-@cache(expire=60*60*24, key_builder=default_key_builder)
+@cache(expire=60*60*24)
 async def get_history_by_vin(lot_id: int = Query(...), site: str = Query(...)):
     data = LotByIDIn(lot_id=lot_id, site=site)
     return await api.request_with_schema(AuctionApiClient.GET_LOT_HISTORY_BY_ID, data)
